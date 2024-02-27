65c7461457e88fe9eeb41c1d3ce75e7f202c4c40
James Michel-Williams <jmichelwilliams@gmail.com>
2024-02-27 01:08:46 -0500

    reverted changes due to misconfigured eslint

--- fetching/src/index.tsx
+++ fetching/src/index.tsx
@@ -2,10 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-const rootElement = document.getElementById('root')
-if (rootElement !== null) {
-  const root = ReactDOM.createRoot(rootElement)
-  root.render(<App />)
-} else {
-  console.error('Root element not found in the document.')
-}
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
