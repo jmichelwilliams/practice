26ff26c5631712a3bd9cf34082a6d61961e9756f
James Michel-Williams <jmichelwilliams@gmail.com>
2024-02-27 23:48:17 -0500

    added ; to code

--- fetching/src/App.tsx
+++ fetching/src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import Homepage from './Homepage'
+import React from 'react';
+import Homepage from './Homepage';
 
 const App: React.FC = () => {
-  return <Homepage />
-}
+  return <Homepage />;
+};
 
-export default App
+export default App;
--- fetching/src/types.ts
+++ fetching/src/types.ts
@@ -1,8 +1,8 @@
 export interface ISSData {
-  message: string
-  timestamp: number
+  message: string;
+  timestamp: number;
   iss_position: {
-    longitude: number
-    latitude: number
-  }
+    longitude: number;
+    latitude: number;
+  };
 }
